c38bacad22a326086e74573673787b0406caba7a
David Kouvchinov <david.kouvchinov@sund.ku.dk>
2024-02-11 21:06:42 -0500

    updated ignore and getting scripts online

---
+++ Untitled-1.sh
@@ -0,0 +1,3 @@
+$MCT/RDKitEnumerateStereoisomers.py -d no -m UnassignedOnly --overwrite --workingdir <stereochemistry dir> -i Sample.smi -o SampleOut.smi
+
+$MCT/RDKitEnumerateStereoisomers.py -d no -m UnassignedOnly --overwrite --workingdir /Users/lkv206/work/to_do_projects/chembl_ligands/GPCR-Bench-master/5HT1B/stereochemistry -i /Users/lkv206/work/to_do_projects/chembl_ligands/GPCR-Bench-master/5HT1B/5HT1B_chembl19_set_active.smi -o /Users/lkv206/work/to_do_projects/chembl_ligands/GPCR-Bench-master/5HT1B/stereochemistry/SampleOut.smi 2>&1 /Users/lkv206/work/to_do_projects/chembl_ligands/GPCR-Bench-master/5HT1B/stereochemistry/SampleOut.log
