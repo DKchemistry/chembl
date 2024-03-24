4950afadc859e04fdf4569c3a41bf2cfc8d127ab
David Kouvchinov <david.kouvchinov@sund.ku.dk>
2024-03-24 19:24:59 -0400

    updated sh scripts because i used different amount of -NJOBS when dealing with re runs

--- grids_lit-pcba/TP53/TP53_3zme_inactive.sh
+++ grids_lit-pcba/TP53/TP53_3zme_inactive.sh
@@ -1,1 +1,1 @@
-/mnt/data/dk/Schrodinger_adv_2021_1/glide -HOST localhost:100 -NJOBS 450 -OVERWRITE -JOBNAME TP53_3zme_inactive_glide TP53_3zme_inactive.in
+/mnt/data/dk/Schrodinger_adv_2021_1/glide -HOST localhost:50 -NJOBS 50 -OVERWRITE -JOBNAME TP53_3zme_inactive_glide TP53_3zme_inactive.in
