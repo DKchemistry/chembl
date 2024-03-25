af83aff1fba19cfc45d32e51c93d74411ad59dd9
David Kouvchinov <david.kouvchinov@sund.ku.dk>
2024-03-25 18:35:59 -0400

    deleted code i don't think i need

--- grids_lit-pcba/papermill.py
+++ grids_lit-pcba/papermill.py
@@ -77,75 +77,6 @@ print(file_id)
 # %%
 
 parameters_list = []
-parameters_dict = {}
-
-for id in file_id:
-    parameters_list = []
-    parameters_list.append(
-        os.path.abspath(
-            os.path.join("papermill", "notebooks", (id + "_litpcba_papermill.ipynb"))
-        )
-    )
-    # print(parameters_list)
-
-    for dirpath, dirname, filenames in os.walk("."):
-        for filename in filenames:
-            if filename.endswith(f"{id}" + "_active_glide_lib_sorted.sdf"):
-                parameters_list.append(os.path.abspath(filename))
-            if filename.endswith(f"{id}" + "_inactive_glide_lib_sorted.sdf"):
-                parameters_list.append(os.path.abspath(filename))
-            if filename.endswith(f"{id}" + "_active_glide_lib_sorted.csv"):
-                parameters_list.append(
-                    os.path.abspath(os.path.join("strain", filename))
-                )
-            if filename.endswith(f"{id}" + "_inactive_glide_lib_sorted.csv"):
-                parameters_list.append(
-                    os.path.abspath(os.path.join("strain", filename))
-                )
-            parameters_dict[id] = parameters_list
-
-for key, value in parameters_dict.items():
-    print(key)
-    for items in value:
-        print(items)
-    print("\n")
-
-# %%
-
-parameters_list = []
-parameters_dict = {}
-
-for id in file_id:
-    # set the key to the id and the value to an empty dictionary
-    parameters_dict[id] = {}
-    parameters_dict[id]["title_suffix"] = id
-    parameters_dict[id]["output_notebook"] = os.path.abspath(
-        os.path.join("papermill", "notebooks", (id + "_litpcba_papermill.ipynb"))
-    )
-
-    for dirpath, dirname, filenames in os.walk("."):
-        for filename in filenames:
-            if filename.endswith(f"{id}" + "_active_glide_lib_sorted.sdf"):
-                parameters_dict[id]["file_path_sdf_active"]=(os.path.abspath(filename))
-            if filename.endswith(f"{id}" + "_inactive_glide_lib_sorted.sdf"):
-                parameters_dict[id]["file_path_sdf_inactive"] = os.path.abspath(filename)
-            if filename.endswith(f"{id}" + "_active_glide_lib_sorted.csv"):
-                parameters_dict[id]["file_path_strain_active"] = os.path.abspath(
-                    os.path.join("strain", filename)
-                )
-            if filename.endswith(f"{id}" + "_inactive_glide_lib_sorted.csv"):
-                parameters_dict[id]["file_path_strain_inactive:"] = os.path.abspath(
-                    os.path.join("strain", filename)
-                )
-
-for key, value in parameters_dict.items():
-    print(key)
-    for key, value in value.items():
-        print(key, value)
-    print("\n")
-# %%
-
-parameters_list = []
 
 for id in file_id:
     parameters_dict = {}
@@ -184,44 +115,40 @@ print(len(parameters_list))
 # print(parameters_list[0])
 # %%
 
-for parameter_key, parameter_value in parameters_dict.items():
-    print(parameter_key, parameter_value)
-    print("litpcba_papermill.ipynb", parameter_key["output_notebook"], parameter_value)
+for params in parameters_list:
+    print("litpcba_papermill.ipynb", params["output_notebook"], params["parameters"])
     # pm.execute_notebook("litpcba_papermill.ipynb", parameter_value["output_notebook"], parameter_value)
 
-print(parameters_dict[id])
-for value in parameters_dict[id].values():
-    print(value)
 # %%
 
 # %%
 
-# Get a list of all subfolders in the current working directory that start with a capital letter
-subfolders = [f.name for f in os.scandir(".") if f.is_dir() and f.name[0].isupper()]
+# # Get a list of all subfolders in the current working directory that start with a capital letter
+# subfolders = [f.name for f in os.scandir(".") if f.is_dir() and f.name[0].isupper()]
 
-parameters_list = []
+# parameters_list = []
 
-# Create a parameters dictionary for each subfolder
-for subfolder in subfolders:
-    parameters = {
-        "title_suffix": subfolder,
-        "file_path_sdf_active": f"./{subfolder}/docking/{subfolder}_active_docking_lib_sorted.sdf",
-        "file_path_sdf_decoy": f"./{subfolder}/docking/{subfolder}_decoy_docking_lib_sorted.sdf",
-        "file_path_strain_active": f"./{subfolder}/strain/{subfolder}_active_docking_lib_sorted.csv",
-        "file_path_strain_decoy": f"./{subfolder}/strain/{subfolder}_decoy_docking_lib_sorted.csv",
-    }
+# # Create a parameters dictionary for each subfolder
+# for subfolder in subfolders:
+#     parameters = {
+#         "title_suffix": subfolder,
+#         "file_path_sdf_active": f"./{subfolder}/docking/{subfolder}_active_docking_lib_sorted.sdf",
+#         "file_path_sdf_decoy": f"./{subfolder}/docking/{subfolder}_decoy_docking_lib_sorted.sdf",
+#         "file_path_strain_active": f"./{subfolder}/strain/{subfolder}_active_docking_lib_sorted.csv",
+#         "file_path_strain_decoy": f"./{subfolder}/strain/{subfolder}_decoy_docking_lib_sorted.csv",
+#     }
 
-    output_notebook = f"./papermill/notebooks/gpcr_papermill_output_{parameters['title_suffix']}.ipynb"
+#     output_notebook = f"./papermill/notebooks/gpcr_papermill_output_{parameters['title_suffix']}.ipynb"
 
-    parameters_list.append(
-        {
-            "output_notebook": output_notebook,
-            "parameters": parameters,
-        }
-    )
+#     parameters_list.append(
+#         {
+#             "output_notebook": output_notebook,
+#             "parameters": parameters,
+#         }
+#     )
 
 
-pprint.pprint(parameters_list)
+# pprint.pprint(parameters_list)
 
 # # Execute the notebook for each set of parameters
 # for params in parameters_list:
@@ -230,8 +157,4 @@ pprint.pprint(parameters_list)
 #         params["output_notebook"],
 #         parameters=params["parameters"],
 #     )
-
-# %%
-pprint.pprint(parameters_list)
-
 # %%
