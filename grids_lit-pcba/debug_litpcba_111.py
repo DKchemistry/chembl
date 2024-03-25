a29fe19461957c57b2991b0b7874c8343a6236e4
David Kouvchinov <david.kouvchinov@sund.ku.dk>
2024-03-25 13:46:46 -0400

    the issue seems to be related to the file paths. when correcting for the erroneous file paths and using the non-multithreaded sdf mol supplier, it works. if you use the wrong paths, itll say invalid file. the multithread does work with correct paths but the error is hard to diagnose when using the wrong paths, because the error is about the threads and not the pathing

--- grids_lit-pcba/debug_litpcba_111.py
+++ grids_lit-pcba/debug_litpcba_111.py
@@ -18,11 +18,12 @@ pd.set_option("display.max_columns", None)
 title_suffix = "ADRB2-4lde"
 
 # Files we are processing
-file_path_sdf_active = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/ADRB2-4lde_active_docking_lib_sorted.sdf"
-file_path_sdf_decoy = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/ADRB2-4lde_inactive_docking_lib_sorted.sdf"
+file_path_sdf_active = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/ADRB2-4lde_active_glide_lib_sorted.sdf"
+file_path_sdf_decoy = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/ADRB2-4lde_inactive_glide_lib_sorted.sdf"
 
-file_path_strain_active = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/strain/ADRB2-4lde_active_docking_lib_sorted_strain.csv"
-file_path_strain_decoy = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/strain/ADRB2-4lde_inactive_docking_lib_sorted_strain.csv"
+file_path_strain_active = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/strain/ADRB2-4lde_active_glide_lib_sorted.csv"
+file_path_strain_decoy = "/Users/lkv206/work/to_do_projects/chembl_ligands/grids_lit-pcba/ADRB2/strain/ADRB2-4lde_inactive_glide_lib_sorted.csv"
+
 
 def sdf_to_df(args):
     """
@@ -38,8 +39,8 @@ def sdf_to_df(args):
     file, active_status = args  # Unpack the tuple of arguments
 
     # Create a molecule supplier
-    # mol_supplier = Chem.MultithreadedSDMolSupplier(file, numWriterThreads=8)
-    mol_supplier = Chem.SDMolSupplier(file)
+    mol_supplier = Chem.MultithreadedSDMolSupplier(file, numWriterThreads=8)
+    #mol_supplier = Chem.SDMolSupplier(file)
 
     # Load the molecules and their properties into a list
     molecules = []
